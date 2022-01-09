0586121deedf67a0047af51f083d616e3aff104b
DevendraChitanna <devendrachitanna@gmail.com>
2022-01-09 13:18:30 -0500

    changed create route to quiz route

---
+++ routes/quiz.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = (db) => {
+  router.get("/new", (req, res) => {
+    res.send("this is a test CREATE!")
+  })
+
+  router.post("/new", (req, res) => {
+    res.send("this is a test CREATE!")
+  })
+  router.get("/:id", (req, res) => {
+    console.log(req.params)
+    const { id } = req.params
+    res.send(`ID ${id}`)
+  })
+  //ADD SQL TO ADD INFO TO DATABASE
+
+  return router;
+};
