00bcd467edf02b03b9bdcf7746187087b31baeb7
canadiankay <kasimnadia@gmail.com>
2022-01-12 07:01:33 +0000

    decided not to show date on homepage but may need to revisit

--- db/seeds/default_quiz_data.sql
+++ db/seeds/default_quiz_data.sql
@@ -1,10 +1,10 @@
 INSERT INTO quiz (question, answer, category, date, Is_public, quiz_url )
- VALUES ('What is the name of the artist who painted ‘Mona Lisa’?', 'Leonardo Da Vinv', 'Art' , NOW(), true, NULL);
+ VALUES ('What is the name of the artist who painted ‘Mona Lisa’?', 'Leonardo Da Vinv', 'Art' , CURRENT_TIMESTAMP, true, NULL);
 
 
  INSERT INTO quiz (question, answer, category, date, Is_public, quiz_url )
- VALUES ('Where is Lincoln Memorial situated in America?', 'Washington D.C', 'Geography' , NOW(), true, NULL);
+ VALUES ('Where is Lincoln Memorial situated in America?', 'Washington D.C', 'Geography' , CURRENT_TIMESTAMP, true, NULL);
 
 
 INSERT INTO quiz (question, answer, category, date, Is_public, quiz_url )
- VALUES ('What is the capital of Brazil?', 'Brasilia', 'Geography' , NOW(), true, NULL);
+ VALUES ('What is the capital of Brazil?', 'Brasilia', 'Geography' , CURRENT_TIMESTAMP, true, NULL);
--- routes/home.js
+++ routes/home.js
@@ -6,7 +6,9 @@ module.exports = (db) => {
   router.get("/home", (req, res) => {
     //res.send("this is a test HOME!")
     console.log("DB QUERY IS RUNNING")
-    db.query("SELECT question, category, date FROM quiz")
+    // db.query("SELECT question, category, date FROM quiz")
+    db.query("SELECT question, category FROM quiz")
+    // since the date is refusing to not show time, decided to not show it on the home page- we shall see if it works out later
       .then(result => {
         console.log(result.rows)
         // console.log("question", result)
