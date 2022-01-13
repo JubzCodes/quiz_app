093f88eec865e07060a408e3b02e9a3e90a62a66
canadiankay <kasimnadia@gmail.com>
2022-01-13 01:18:57 +0000

    added 6 questions in the database in order to generate six columns on the home page

--- db/seeds/default_quiz_data.sql
+++ db/seeds/default_quiz_data.sql
@@ -8,3 +8,15 @@ INSERT INTO quiz (question, answer, category, date, Is_public, quiz_url )
 
 INSERT INTO quiz (question, answer, category, date, Is_public, quiz_url )
  VALUES ('What is the capital of Brazil?', 'Brasilia', 'Geography' , CURRENT_TIMESTAMP, true, NULL);
+
+INSERT INTO quiz (question, answer, category, date, Is_public, quiz_url )
+ VALUES ('What is 2 + 2?', '4', 'Math' , CURRENT_TIMESTAMP, true, NULL);
+
+
+ INSERT INTO quiz (question, answer, category, date, Is_public, quiz_url )
+ VALUES ('In what country is Niagara Falls located?', 'Toronto', 'Geography' , CURRENT_TIMESTAMP, true, NULL);
+
+
+INSERT INTO quiz (question, answer, category, date, Is_public, quiz_url )
+ VALUES ('What was the name of the journalist that co-wrote "The Autobiography of Malcolm X"?', 'Alex Harley', 'Literature' , CURRENT_TIMESTAMP, true, NULL);
+
