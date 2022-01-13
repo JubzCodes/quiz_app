57e9d070d6838f406cee716136fd5055cf39696e
canadiankay <kasimnadia@gmail.com>
2022-01-13 03:50:16 +0000

    event listener that renders messssage based on privacy

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,20 +1,5 @@
 // Client facing scripts here
 
-
-// document.addEventListener("click", () => {
-//   alert("You clicked somewhere on google.com!");
-// });
-
-
-// $(document).ready(function() {
-//   //everytime you click on a column on home page then a message pops up!
-//   $(".column").click(function() {
-//     alert("Hello, world!");
-//   });
-
-// })
-
-
 $(document).ready(function(){
   // this sends a reminder message to the privacy setting on createpage
   $('input[type=radio]').click(function(){
